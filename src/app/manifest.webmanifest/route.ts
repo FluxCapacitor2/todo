5fd797786da39588d78795d709d0f0f1fec8ea36
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2023-05-10 22:11:33 -0400

    Use a custom env var for base URL

--- src/app/manifest.webmanifest/route.ts
+++ src/app/manifest.webmanifest/route.ts
@@ -9,7 +9,7 @@ export const GET = () => {
       "sizes": "512x512"
     }
   ],
-  "start_url": "http://${process.env.NEXT_PUBLIC_VERCEL_URL}/",
+  "start_url": "${process.env.BASE_URL}/",
   "display": "minimal-ui",
   "gcm_sender_id": "890377760325"
 }`);
