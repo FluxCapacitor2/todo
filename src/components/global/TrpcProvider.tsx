49183f8d5e2f8dedf60936d1d9d14de9693c3bb4
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2023-05-14 22:50:58 -0400

    Redirect to signin page when necessary

--- src/components/global/TrpcProvider.tsx
+++ src/components/global/TrpcProvider.tsx
@@ -2,21 +2,52 @@
 
 import { createIDBPersister } from "@/util/idb-persister";
 import { trpc } from "@/util/trpc/trpc";
-import { QueryClient } from "@tanstack/react-query";
+import { AppRouter } from "@/util/trpc/trpc-router";
+import { QueryCache, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
-import { getFetch, httpBatchLink, loggerLink } from "@trpc/client";
+import {
+  TRPCClientError,
+  getFetch,
+  httpBatchLink,
+  loggerLink,
+} from "@trpc/client";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
+import toast from "react-hot-toast";
 import superjson from "superjson";
 
 export const TrpcProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
+  const router = useRouter();
   const [queryClient] = useState(
     () =>
       new QueryClient({
+        queryCache: new QueryCache({
+          onError: (error, query) => {
+            if (
+              (error as TRPCClientError<AppRouter>)?.data?.code ===
+              "UNAUTHORIZED"
+            ) {
+              router.push("/signin");
+              toast.error("You are not signed in!");
+            } else if (query.state.data !== undefined) {
+              toast.error("There was an error updating your tasks.");
+            }
+          },
+        }),
         defaultOptions: {
           queries: {
+            retry: (failureCount, error) => {
+              if (
+                (error as TRPCClientError<AppRouter>)?.data?.code ===
+                "UNAUTHORIZED"
+              ) {
+                return false;
+              }
+              return failureCount < 3;
+            },
             cacheTime: 1000 * 60 * 60 * 24, // 24 hours
             staleTime: 10000, // 10 seconds
             refetchInterval: 1000 * 120, // 2 minutes
