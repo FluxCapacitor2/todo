83f3a2f93a4bdcbb58dbbecf1f2b385cb2499dc6
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2023-05-19 23:53:12 -0400

    Try to fix PWA registration

--- next.config.js
+++ next.config.js
@@ -1,12 +1,43 @@
-const withPWA = require("@imbios/next-pwa")({
+// https://github.com/Schular/next-with-pwa/blob/main/next.config.js
+
+const path = require("path");
+const withPWAInit = require("@imbios/next-pwa");
+
+/** @type {import('@imbios/next-pwa').PWAConfig} */
+const withPWA = withPWAInit({
   dest: "public",
-  customWorkerDir: "src/worker",
+  // Solution: https://github.com/shadowwalker/next-pwa/issues/424#issuecomment-1399683017
+  buildExcludes: ["app-build-manifest.json"],
 });
+
+const generateAppDirEntry = (entry) => {
+  const packagePath = require.resolve("@imbios/next-pwa");
+  const packageDirectory = path.dirname(packagePath);
+  const registerJs = path.join(packageDirectory, "register.js");
+
+  return entry().then((entries) => {
+    // Register SW on App directory, solution: https://github.com/shadowwalker/next-pwa/pull/427
+    if (entries["main-app"] && !entries["main-app"].includes(registerJs)) {
+      if (Array.isArray(entries["main-app"])) {
+        entries["main-app"].unshift(registerJs);
+      } else if (typeof entries["main-app"] === "string") {
+        entries["main-app"] = [registerJs, entries["main-app"]];
+      }
+    }
+    return entries;
+  });
+};
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
+  },
+  webpack: (config) => {
+    const entry = generateAppDirEntry(config.entry);
+    config.entry = () => entry;
+
+    return config;
   },
 };
 
