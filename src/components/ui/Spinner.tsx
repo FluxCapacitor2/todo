10cbbb896f3a893b67f4d07ba227a51e78207a03
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2023-08-09 19:53:45 -0400

    Fix build

--- src/components/ui/Spinner.tsx
+++ src/components/ui/Spinner.tsx
@@ -1,6 +1,6 @@
 import { cn } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
 
-export const Spinner = ({ className }: { className: string }) => (
+export const Spinner = ({ className }: { className?: string }) => (
   <Loader2 className={cn("animate-spin", className)} />
 );
