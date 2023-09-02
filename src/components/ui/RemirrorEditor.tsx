ccafe55b9f5cab625841bed99b4f4425edbe138a
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2023-09-02 18:13:20 -0400

    Hide Remirror editor scroll bar

--- src/components/ui/RemirrorEditor.tsx
+++ src/components/ui/RemirrorEditor.tsx
@@ -63,7 +63,7 @@ export const RemirrorEditor = ({
         initialContent={state}
         onBlur={save}
         classNames={[
-          "w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          "w-full rounded-md !overflow-y-auto border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
         ]}
       />
     </div>
