9dc1152b25c5d79746f8a0e2b76b4a92661cbcac
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2023-09-18 22:40:50 -0400

    Group items in tasks page by due date

--- src/app/(sidebar)/tasks/page.tsx
+++ src/app/(sidebar)/tasks/page.tsx
@@ -4,6 +4,17 @@ import { ProjectSelector } from "@/components/project/ProjectSelector";
 import { TaskCard } from "@/components/task/TaskCard";
 import { sortByDueDate } from "@/util/sort";
 import { trpc } from "@/util/trpc/trpc";
+import {
+  addMonths,
+  addWeeks,
+  endOfTomorrow,
+  isBefore,
+  startOfMonth,
+  startOfWeek,
+  startOfYear,
+} from "date-fns";
+import groupBy from "just-group-by";
+import { Fragment } from "react";
 import { ListSkeleton } from "../(perProject)/list/[id]/ListSkeleton";
 
 export default function Page() {
@@ -19,16 +30,75 @@ export default function Page() {
                 included?.some((it) => it.id === task.projectId)
               )
             : undefined;
+
+          const groups = filtered
+            ? groupBy(filtered, (task): string => {
+                if (!task.dueDate) {
+                  return "No Due Date";
+                }
+
+                const date = task.dueDate;
+                const now = new Date();
+
+                if (date < now) {
+                  return "Overdue";
+                }
+
+                if (isBefore(date, endOfTomorrow())) {
+                  return "Tomorrow";
+                }
+
+                if (
+                  startOfWeek(now).getTime() === startOfWeek(date).getTime()
+                ) {
+                  return "This Week";
+                }
+
+                if (
+                  startOfMonth(now).getTime() === startOfMonth(date).getTime()
+                ) {
+                  return "This Month";
+                }
+
+                if (
+                  startOfWeek(addWeeks(now, 1)).getTime() ===
+                  startOfWeek(date).getTime()
+                ) {
+                  return "Next Week";
+                }
+
+                if (
+                  startOfMonth(addMonths(now, 1)).getTime() ===
+                  startOfMonth(date).getTime()
+                ) {
+                  return "Next Month";
+                }
+
+                if (
+                  startOfYear(now).getTime() === startOfYear(date).getTime()
+                ) {
+                  return "This Year";
+                }
+
+                return "Beyond";
+              })
+            : undefined;
+
           return (
             <>
-              {isLoading || !filtered ? (
+              {isLoading || !groups ? (
                 <ListSkeleton />
               ) : (
                 <>
-                  {filtered.map((task) => (
-                    <div className="p-2" key={task.id}>
-                      <TaskCard task={task} isListItem details />
-                    </div>
+                  {Object.keys(groups).map((group) => (
+                    <Fragment key={group}>
+                      <h2 className="text-2xl font-bold">{group}</h2>
+                      {groups[group]!.map((task) => (
+                        <div className="p-2" key={task.id}>
+                          <TaskCard task={task} isListItem details />
+                        </div>
+                      ))}
+                    </Fragment>
                   ))}
                 </>
               )}
